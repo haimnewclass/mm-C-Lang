4fa955f77bc1557477e76e38421396e9081a4d01
haim hod gilboa <haim@newclass.co.il>
2022-09-18 15:14:23 +0300

    tree

--- mm-C-Lang/Week6_1_Tree1.c
+++ mm-C-Lang/Week6_1_Tree1.c
@@ -20,18 +20,32 @@ typedef struct Leaf
 	// Data (Real Data)
 	DetailsT Details;
 } TLeaf;
-struct Leaf* Root;
-struct Leaf* Head;
+struct Leaf* Root; 
 
 typedef struct QueueItem {
 	TLeaf *leaf;
+
+	struct QueueItem* prev;
+	struct QueueItem* next;
 
 } TQueueItem;
 
 TQueueItem *Queue_Head;
 TQueueItem *Queue_Tail;
 
-void Queue_Push(TQueueItem* item)
+void Queue_Push(TLeaf* leaf)
+{
+	//malloc
+}
+
+
+void Queue_Status()
+{
+
+}
+
+
+void Queue_Clear()
 {
 
 }
@@ -40,18 +54,78 @@ TQueueItem*  Queue_Pop()
 {
 	 
 }
+
 //////////////////////////////////////
-void AddItem(DetailsT item)
+void AddTreeItem(DetailsT item)
 {
+	// Add new item into Tree
+	struct Leaf* newLeaf = (struct Leaf*)malloc(sizeof(struct Leaf));
+	newLeaf->Details = item;
+
+	newLeaf->Right = NULL;
+	newLeaf->Left = NULL;
+	
+	if (Root == NULL)
+	{
+		newLeaf->Parent = NULL;
+		Root = newLeaf;
+		newLeaf->level = 0;
+	}
+	else if (Root->Right == NULL)
+	{
+		Root->Right = newLeaf;
+		newLeaf->Parent = Root;
+		newLeaf->level = 1;
+	}
+	else if (Root->Left == NULL)
+	{
+		Root->Left = newLeaf;
+		newLeaf->Parent = Root;
+		newLeaf->level = 1;
+	}
+
 
 }
+
+
+void BuildTree()
+{
+	for (int i = 0; i < 100; i++)
+	{
+		DetailsT* details = (DetailsT*)malloc(sizeof(DetailsT));
+		details->age = i;
+
+		AddTreeItem(*details);
+	}
+}
+
+void FromQueueToTree()
+{
+	 
+}
+
+TLeaf* FindEmptyPlaceLeaf(TLeaf* leaf)
+{
+	if (leaf->Left != NULL)
+		return leaf->Left;
+	else if (leaf->Right != NULL)
+		return leaf->Right;
+	else
+		return NULL;
+}
+
 main()
 {
+	Root = NULL;
+	Queue_Tail = NULL;
+	Queue_Head = NULL;
+
+	BuildTree();
 	struct Leaf leaf;
 	struct Leaf *leafPtr = (struct Leaf* ) malloc(sizeof(struct Leaf));
 
 	leaf.Details.age = 23;
 	leafPtr->Details.age = 23;
 
-	strcpy(leaf.Details.address, "dafsdfsdf");
+	//strcpy(leaf.Details.address, "dafsdfsdf");
 }
